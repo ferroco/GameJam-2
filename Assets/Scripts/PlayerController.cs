b196847bf9f5697a4e7ff821c9182b5d7d1519a3
nicoDev32b <diegonicolas.dev@gmail.com>
2025-05-19 23:10:26 -0500

    Squashed commit of the following:
    commit 8b88457c49577dc242e5796fe05916d56d3289e5
    Merge: 6f7af0f 2766ffb
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 21:23:14 2025 -0500
    
        Merge branch 'Sergio2' into Angela
    
    commit 6f7af0fb24bffb0906212be3eef8ada0895cf68b
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 21:15:51 2025 -0500
    
        upload
    
    commit 2766ffb86050a782949418d6af9c112533e61b3f
    Author: Gieros99 <sacvx99@gmail.com>
    Date:   Mon May 19 21:13:51 2025 -0500
    
        Animacion Conejito
    
    commit 51e8c66da8c12acc3eb7a3a3236c6af5581d949e
    Merge: 154a9d5 4a2989a
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 17:33:48 2025 -0500
    
        Merge branch 'Nico' into Angela
    
    commit 154a9d57ba3bcb79307eb27b1448b01887ab45bd
    Merge: 70ec6b2 d85ebba
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 15:26:54 2025 -0500
    
        Merge branch 'Sergio2' into Angela
    
    commit d85ebba515d4793bf5a9f7dc547d7df06d908ddc
    Author: Gieros99 <sacvx99@gmail.com>
    Date:   Mon May 19 15:25:13 2025 -0500
    
        trabajo movimiento, empujar
    
    commit 70ec6b228605782e5a6e76347703ddeae2e4ceeb
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 14:42:36 2025 -0500
    
        arboles
    
    commit 917f5381341a27d53a347571917356168d341205
    Merge: a7d717a 8257f53
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 13:23:03 2025 -0500
    
        Merge branch 'Nico' into Angela
    
    commit a7d717a78830cd1f16e62e936796d4b486c60550
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 13:20:24 2025 -0500
    
        arbolesprefab
    
    commit 6f8534f5c29c2d5d532e03c3fa2c056b5123b0a7
    Merge: 79f2a0a 20d2818
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 12:42:12 2025 -0500
    
        Merge branch 'Nico' into Angela
    
    commit 79f2a0a2a257609cf90065fd037b73ec34c5bb63
    Merge: e2cadbe a9adafc
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 12:40:14 2025 -0500
    
        Merge remote-tracking branch 'origin/Nico' into Angela
    
    commit e2cadbe9481bb7154be9411f56f160e7c7f7f8da
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 11:43:56 2025 -0500
    
        upload asset
    
    commit b8004aa184dedc3918c086e8b6528203a35c93c8
    Merge: ba33716 5025257
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 11:28:02 2025 -0500
    
        Merge remote-tracking branch 'origin/Nico' into Angela
    
    commit ba337164ba24de4e9e2de1a7afd495039510014b
    Author: agfonse2 <130771627+agfonse2@users.noreply.github.com>
    Date:   Mon May 19 11:27:46 2025 -0500
    
        scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System.Collections;
+
+public class TilePlayerController : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    public float runSpeed = 5f;
+    public float tileSize = 1f;
+    public LayerMask obstacleLayer;
+    public LayerMask pushableLayer;
+
+    private bool isMoving = false;
+    private Vector2 input;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+                Vector3 destination = transform.position + new Vector3(input.x, input.y, 0f) * tileSize;
+
+                // Check for pushable object
+                Collider2D pushable = Physics2D.OverlapCircle(destination, 0.1f, pushableLayer);
+                if (pushable != null)
+                {
+                    PushableObject po = pushable.GetComponent<PushableObject>();
+                    if (po != null && po.CanMove(input))
+                    {
+                        StartCoroutine(PushAndMove(po, input, destination));
+                        return;
+                    }
+                }
+
+                // Move player if there's no obstacle
+                if (!Physics2D.OverlapCircle(destination, 0.1f, obstacleLayer | pushableLayer))
+                {
+                    float speed = Input.GetKey(KeyCode.X) ? runSpeed : walkSpeed;
+                    StartCoroutine(MoveTo(destination, speed));
+                }
+            }
+        }
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator PushAndMove(PushableObject obj, Vector2 dir, Vector3 playerDestination)
+    {
+        isMoving = true;
+
+        // Start push coroutine
+        StartCoroutine(obj.Move(dir));
+
+        // Move player after a short delay to match object push timing
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        while ((playerDestination - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerDestination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = playerDestination;
+        isMoving = false;
+    }
+
+    IEnumerator MoveTo(Vector3 destination, float speed)
+    {
+        isMoving = true;
+
+        while ((destination - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = destination;
+        isMoving = false;
+    }
+}
---
+++ Assets/Scripts/PushableObject.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class PushableObject : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float tileSize = 1f;
+    public LayerMask obstacleLayer;
+
+    private bool isMoving = false;
+
+    public bool CanMove(Vector2 direction)
+    {
+        Vector3 destination = transform.position + (Vector3)(direction * tileSize);
+
+        // Check if the destination has any obstacle or another pushable
+        return !Physics2D.OverlapCircle(destination, 0.1f, obstacleLayer);
+    }
+
+    public IEnumerator Move(Vector2 direction)
+    {
+        isMoving = true;
+        Vector3 destination = transform.position + (Vector3)(direction * tileSize);
+
+        while ((destination - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = destination;
+        isMoving = false;
+    }
+}
